e67e5400d759b52de26f48146ddf862f3787f49b
pilao <zeffy@users.noreply.github.com>
2021-03-02 09:03:21 -0800

    make xmlpatch element names case-sensitive (and node keys also)
    this is because pugixml functions are case sensitive, so behavior was largely inconsistent before

--- src/bnspatch/xmlpatch.cpp
+++ src/bnspatch/xmlpatch.cpp
@@ -90,12 +90,12 @@ void preprocess(pugi::xml_document &patches_doc, const std::filesystem::path &pa
   pugi::xml_document document;
   if ( include_guard.emplace(std::move(path.wstring())).second
     && try_load_file(document, path, pugi::parse_default | pugi::parse_pi)
-    && !_wcsicmp(document.document_element().name(), L"patches") ) {
+    && std::wcscmp(document.document_element().name(), L"patches") == 0 ) {
 
     for ( const auto &child : document.children() ) {
       switch ( child.type() ) {
         case pugi::node_element:
-          if ( !_wcsicmp(child.name(), L"patches") ) {
+          if ( std::wcscmp(child.name(), L"patches") == 0 ) {
             patches_doc.document_element().append_child(pugi::node_comment).set_value(path.c_str());
             for ( const auto &patch : child.children(L"patch") ) {
               if ( patch.attribute(L"enabled").as_bool(true) )
@@ -105,7 +105,7 @@ void preprocess(pugi::xml_document &patches_doc, const std::filesystem::path &pa
           break;
 
         case pugi::node_pi:
-          if ( !_wcsicmp(child.name(), L"include") ) {
+          if ( std::wcscmp(child.name(), L"include") == 0 ) {
             std::wstring result;
             if ( FAILED(wil::ExpandEnvironmentStringsW(child.value(), result)) )
               continue;
@@ -147,27 +147,27 @@ void patch_node(
   for ( const auto &current : children ) {
     if ( ctx.attribute() ) {
       switch ( fnv1a::make_hash(current.name(), fnv1a::ascii_toupper) ) {
-        case L"parent"_fnv1au: // ok
+        case L"parent"_fnv1a: // ok
           patch_node(doc, encoding, ctx.parent(), current.children(), node_keys);
           break;
 
-        case L"set-name"_fnv1au: // ok
+        case L"set-name"_fnv1a: // ok
           ctx.attribute().set_name(current.attribute(L"name").value());
           break;
 
-        case L"set-value"_fnv1au: // ok
+        case L"set-value"_fnv1a: // ok
           ctx.attribute().set_value(current.attribute(L"value").value());
           break;
 
-        case L"previous-attribute"_fnv1au: // ok
+        case L"previous-attribute"_fnv1a: // ok
           patch_node(doc, encoding, pugi::xpath_node(ctx.attribute().previous_attribute(), ctx.parent()), current.children(), node_keys);
           break;
 
-        case L"next-attribute"_fnv1au: // ok
+        case L"next-attribute"_fnv1a: // ok
           patch_node(doc, encoding, pugi::xpath_node(ctx.attribute().next_attribute(), ctx.parent()), current.children(), node_keys);
           break;
 
-        case L"insert-attribute-before"_fnv1au: { // ok
+        case L"insert-attribute-before"_fnv1a: { // ok
           ctx.node().remove_attribute(current.attribute(L"name").value());
           auto attribute = ctx.node().insert_attribute_before(current.attribute(L"name").value(), ctx.attribute());
           if ( const auto value = current.attribute(L"value") )
@@ -175,7 +175,7 @@ void patch_node(
           patch_node(doc, encoding, pugi::xpath_node(attribute, ctx.node()), current.children(), node_keys);
           break;
         }
-        case L"insert-attribute-after"_fnv1au: { // ok
+        case L"insert-attribute-after"_fnv1a: { // ok
           ctx.node().remove_attribute(current.attribute(L"name").value());
           auto attribute = ctx.node().insert_attribute_after(current.attribute(L"name").value(), ctx.attribute());
           if ( const auto value = current.attribute(L"value") )
@@ -183,51 +183,51 @@ void patch_node(
           patch_node(doc, encoding, pugi::xpath_node(attribute, ctx.node()), current.children(), node_keys);
           break;
         }
-        case L"remove"_fnv1au: // ok
+        case L"remove"_fnv1a: // ok
           ctx.parent().remove_attribute(ctx.attribute());
           return;
       }
     } else {
       switch ( fnv1a::make_hash(current.name(), fnv1a::ascii_toupper) ) {
-        case L"save-file"_fnv1au:
+        case L"save-file"_fnv1a:
           if ( ctx.node().type() == pugi::node_document )
             doc.save_file(current.attribute(L"path").value(), L"", pugi::format_default | pugi::format_no_declaration, encoding);
           break;
 
-        case L"load-file"_fnv1au:
+        case L"load-file"_fnv1a:
           if ( ctx.node().type() == pugi::node_document )
             doc.load_file(current.attribute(L"path").value(), pugi::parse_full);
           break;
 
-        case L"load-buffer"_fnv1au:
+        case L"load-buffer"_fnv1a:
           if ( ctx.node().type() == pugi::node_document )
             doc.load_string(current.text().get());
           break;
 
-        case L"reset"_fnv1au:
+        case L"reset"_fnv1a:
           if ( ctx.node().type() == pugi::node_document )
             doc.reset();
           break;
 
-        case L"parent"_fnv1au: // ok
+        case L"parent"_fnv1a: // ok
           patch_node(doc, encoding, ctx.parent(), current.children(), node_keys);
           break;
 
-        case L"select-node"_fnv1au: // ok
+        case L"select-node"_fnv1a: // ok
           patch_node(doc, encoding, ctx.node().select_node(current.attribute(L"query").value()), current.children(), node_keys);
           break;
 
-        case L"select-nodes"_fnv1au: // ok
+        case L"select-nodes"_fnv1a: // ok
           for ( const auto &node : ctx.node().select_nodes(current.attribute(L"query").value()) )
             patch_node(doc, encoding, node, current.children(), node_keys);
           break;
 
-        case L"if"_fnv1au:
+        case L"if"_fnv1a:
           if ( ctx.node().select_node(current.attribute(L"query").value()) )
             patch_node(doc, encoding, ctx.node(), current.children(), node_keys);
           break;
 
-        case L"prepend-attribute"_fnv1au: { // ok
+        case L"prepend-attribute"_fnv1a: { // ok
           ctx.node().remove_attribute(current.attribute(L"name").value());
           auto attribute = ctx.node().prepend_attribute(current.attribute(L"name").value());
           if ( const auto value = current.attribute(L"value") )
@@ -235,7 +235,7 @@ void patch_node(
           patch_node(doc, encoding, pugi::xpath_node(attribute, ctx.node()), current.children(), node_keys);
           break;
         }
-        case L"append-attribute"_fnv1au: { // ok
+        case L"append-attribute"_fnv1a: { // ok
           ctx.node().remove_attribute(current.attribute(L"name").value());
           auto attribute = ctx.node().append_attribute(current.attribute(L"name").value());
           if ( const auto value = current.attribute(L"value") )
@@ -243,52 +243,52 @@ void patch_node(
           patch_node(doc, encoding, pugi::xpath_node(attribute, ctx.node()), current.children(), node_keys);
           break;
         }
-        case L"prepend-child"_fnv1au: // ok
+        case L"prepend-child"_fnv1a: // ok
           patch_node(doc, encoding, ctx.node().prepend_child(current.attribute(L"name").value()), current.children(), node_keys);
           break;
 
-        case L"append-buffer"_fnv1au:  // ok
+        case L"append-buffer"_fnv1a:  // ok
           ctx.node().append_buffer(current.text().get(), wcslen(current.text().get()) * sizeof(wchar_t), pugi::parse_full | pugi::parse_fragment, pugi::encoding_utf16);
           break;
 
-        case L"append-child"_fnv1au: // ok
+        case L"append-child"_fnv1a: // ok
           patch_node(doc, encoding, ctx.node().append_child(current.attribute(L"name").value()), current.children(), node_keys);
           break;
 
-        case L"prepend-copy"_fnv1au: // ok
+        case L"prepend-copy"_fnv1a: // ok
           if ( const auto it = node_keys.find(current.attribute(L"node-key").value()); it != node_keys.end() )
             patch_node(doc, encoding, ctx.node().prepend_copy(it->second), current.children(), node_keys);
           break;
 
-        case L"append-copy"_fnv1au: // ok
+        case L"append-copy"_fnv1a: // ok
           if ( const auto it = node_keys.find(current.attribute(L"node-key").value()); it != node_keys.end() )
             patch_node(doc, encoding, ctx.node().append_copy(it->second), current.children(), node_keys);
           break;
 
-        case L"prepend-move"_fnv1au: // ok
+        case L"prepend-move"_fnv1a: // ok
           if ( const auto it = node_keys.find(current.attribute(L"node-key").value()); it != node_keys.end() )
             patch_node(doc, encoding, ctx.node().prepend_move(it->second), current.children(), node_keys);
           break;
 
-        case L"append-move"_fnv1au: // ok
+        case L"append-move"_fnv1a: // ok
           if ( const auto it = node_keys.find(current.attribute(L"node-key").value()); it != node_keys.end() )
             patch_node(doc, encoding, ctx.node().append_move(it->second), current.children(), node_keys);
           break;
 
-        case L"attribute"_fnv1au: // ok
+        case L"attribute"_fnv1a: // ok
           patch_node(doc, encoding, pugi::xpath_node(ctx.node().attribute(current.attribute(L"name").value()), ctx.node()), current.children(), node_keys);
           break;
 
-        case L"attributes"_fnv1au: // ok
+        case L"attributes"_fnv1a: // ok
           for ( const auto &attr : ctx.node().attributes() )
             patch_node(doc, encoding, pugi::xpath_node(attr, ctx.node()), current.children(), node_keys);
           break;
 
-        case L"child"_fnv1au: // ok
+        case L"child"_fnv1a: // ok
           patch_node(doc, encoding, ctx.node().child(current.attribute(L"name").value()), current.children(), node_keys);
           break;
 
-        case L"children"_fnv1au: // ok
+        case L"children"_fnv1a: // ok
           if ( const auto name = current.attribute(L"name") ) {
             for ( const auto &child : ctx.node().children(name.value()) )
               patch_node(doc, encoding, child, current.children(), node_keys);
@@ -298,7 +298,7 @@ void patch_node(
           }
           break;
 
-        case L"find-child-by-attribute"_fnv1au: // ok
+        case L"find-child-by-attribute"_fnv1a: // ok
           if ( const auto name = current.attribute(L"name") ) {
             patch_node(doc, encoding, ctx.node().find_child_by_attribute(
               name.value(),
@@ -313,91 +313,91 @@ void patch_node(
           }
           break;
 
-        case L"first-attribute"_fnv1au: // ok
+        case L"first-attribute"_fnv1a: // ok
           patch_node(doc, encoding, pugi::xpath_node(ctx.node().first_attribute(), ctx.node()), current.children(), node_keys);
           break;
 
-        case L"last-attribute"_fnv1au: // ok
+        case L"last-attribute"_fnv1a: // ok
           patch_node(doc, encoding, pugi::xpath_node(ctx.node().last_attribute(), ctx.node()), current.children(), node_keys);
           break;
 
-        case L"first-child"_fnv1au: // ok
+        case L"first-child"_fnv1a: // ok
           patch_node(doc, encoding, ctx.node().first_child(), current.children(), node_keys);
           break;
 
-        case L"last-child"_fnv1au: // ok
+        case L"last-child"_fnv1a: // ok
           patch_node(doc, encoding, ctx.node().last_child(), current.children(), node_keys);
           break;
 
-        case L"first-element-by-path"_fnv1au: // ok
+        case L"first-element-by-path"_fnv1a: // ok
           patch_node(doc, encoding, ctx.node().first_element_by_path(current.attribute(L"path").value()), current.children(), node_keys);
           break;
 
-        case L"insert-sibling-after"_fnv1au: // ok
+        case L"insert-sibling-after"_fnv1a: // ok
           patch_node(doc, encoding, ctx.node().parent().insert_child_after(current.attribute(L"name").value(), ctx.node()), current.children(), node_keys);
           break;
 
-        case L"insert-sibling-before"_fnv1au: // ok
+        case L"insert-sibling-before"_fnv1a: // ok
           patch_node(doc, encoding, ctx.node().parent().insert_child_before(current.attribute(L"name").value(), ctx.node()), current.children(), node_keys);
           break;
 
-        case L"insert-copy-after"_fnv1au: // ok
+        case L"insert-copy-after"_fnv1a: // ok
           if ( const auto it = node_keys.find(current.attribute(L"node-key").value()); it != node_keys.end() )
             patch_node(doc, encoding, it->second.parent().insert_move_before(ctx.node(), it->second), current.children(), node_keys);
           break;
 
-        case L"insert-copy-before"_fnv1au: // ok
+        case L"insert-copy-before"_fnv1a: // ok
           if ( const auto it = node_keys.find(current.attribute(L"node-key").value()); it != node_keys.end() )
             patch_node(doc, encoding, it->second.parent().insert_copy_before(ctx.node(), it->second), current.children(), node_keys);
           break;
 
-        case L"insert-move-after"_fnv1au: // ok
+        case L"insert-move-after"_fnv1a: // ok
           if ( const auto it = node_keys.find(current.attribute(L"node-key").value()); it != node_keys.end() )
             patch_node(doc, encoding, it->second.parent().insert_move_after(ctx.node(), it->second), current.children(), node_keys);
           break;
 
-        case L"insert-move-before"_fnv1au: // ok
+        case L"insert-move-before"_fnv1a: // ok
           if ( const auto it = node_keys.find(current.attribute(L"node-key").value()); it != node_keys.end() )
             patch_node(doc, encoding, it->second.parent().insert_move_before(ctx.node(), it->second), current.children(), node_keys);
           break;
 
-        case L"next-sibling"_fnv1au: // ok
+        case L"next-sibling"_fnv1a: // ok
           patch_node(doc, encoding, ctx.node().next_sibling(), current.children(), node_keys);
           break;
 
-        case L"remove-attribute"_fnv1au: // ok
+        case L"remove-attribute"_fnv1a: // ok
           ctx.node().remove_attribute(current.attribute(L"name").value());
           break;
 
-        case L"remove-attributes"_fnv1au: // ok
+        case L"remove-attributes"_fnv1a: // ok
           ctx.node().remove_attributes();
           break;
 
-        case L"remove-child"_fnv1au: // ok
+        case L"remove-child"_fnv1a: // ok
           ctx.node().remove_child(current.attribute(L"name").value());
           break;
 
-        case L"remove-children"_fnv1au: // ok
+        case L"remove-children"_fnv1a: // ok
           ctx.node().remove_children();
           break;
 
-        case L"root"_fnv1au: // ok
+        case L"root"_fnv1a: // ok
           patch_node(doc, encoding, ctx.node().root(), current.children(), node_keys);
           break;
 
-        case L"set-name"_fnv1au: // ok
+        case L"set-name"_fnv1a: // ok
           ctx.node().set_name(current.attribute(L"name").value());
           break;
 
-        case L"set-value"_fnv1au: // ok
+        case L"set-value"_fnv1a: // ok
           ctx.node().set_value(current.attribute(L"value").value());
           break;
 
-        case L"assign-node-key"_fnv1au: // ok
+        case L"assign-node-key"_fnv1a: // ok
           node_keys.insert_or_assign(current.attribute(L"node-key").value(), ctx.node());
           break;
 
-        case L"remove"_fnv1au: // ok
+        case L"remove"_fnv1a: // ok
           ctx.node().parent().remove_child(ctx.node());
           return;
       }
