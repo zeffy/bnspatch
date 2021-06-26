8ac45fd18071debb716fefaaeb14d345b0186c3b
皮老 <zeffy@users.noreply.github.com>
2021-06-26 03:47:00 -0700

    Update callbacks.cpp

--- src/template/callbacks.cpp
+++ src/template/callbacks.cpp
@@ -1,7 +1,7 @@
 #include "pch.h"
 #include "pluginsdk.h"
 
-bool  __cdecl oep_notify([[maybe_unused]] const Version version)
+bool  __cdecl init([[maybe_unused]] const Version version)
 {
   // do stuff
   return true;
@@ -17,6 +17,7 @@ extern "C" __declspec(dllexport) PluginInfo GPluginInfo = {
   .hide_from_peb = true,
   .erase_pe_header = true,
 #endif
+  .init = init,
   .oep_notify = oep_notify,
   .priority = 1,
   .target_apps = L"Client.exe;BNSR.exe"
